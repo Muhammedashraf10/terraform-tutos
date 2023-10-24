21bc66d9b60197a8c1c0c0f28769babba296ad06
Muhammedashraf10 <muhammedashraf192@gmail.com>
2023-10-24 11:44:46 +0300

    Update vars.tf

--- vpc-creation/vars.tf
+++ vpc-creation/vars.tf
@@ -1,9 +1,17 @@
 variable "aws_region" {
-  
+  type    = string
+  description = "this is the regio whic"
   default = "us-east-1"
+  validation {
+    condition =  can(regex("^us-", var.aws_region))
+    error_message = "this region is not in the USA region"
+  }
 }
+
 variable "environment" {
-  default = "javatodev"
+  type = string
+  description = "this is the vpc name which will be used"
+  default = "myvpc-terraform"
 }
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
@@ -15,3 +23,17 @@ provider "aws" {
 locals {
   availability_zones = ["${var.aws_region}a", "${var.aws_region}b"]
 }
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.0.0/20", "10.0.128.0/20"]
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.16.0/20", "10.0.144.0/20"]
+  description = "CIDR block for Private Subnet"
+}
+
+
